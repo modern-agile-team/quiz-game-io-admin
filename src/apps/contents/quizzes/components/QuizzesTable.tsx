a6805fe8620767ee34237e967fac98a4e40c6dc5
zzzryt <jinjinstar3@gmail.com>
2025-11-12 15:05:39 +0900

    style: imageFileName 변경 #162
    현재 Dto가 잘못설정되어있어서, 추후 변경 후 다시 적용

--- src/apps/contents/quizzes/components/QuizzesTable.tsx
+++ src/apps/contents/quizzes/components/QuizzesTable.tsx
@@ -14,7 +14,7 @@ export default function QuizzesTable() {
   const { imageId } = useSearch({ from: '/(menus)/contents/quizzes/' });
   const { data: image } = useSuspenseQuery(imageQueries.getSingle(imageId));
   const { data: quizzes } = useSuspenseQuery({
-    ...quizQueries.getList({ imageFileName: image.quizImageFileName! }),
+    ...quizQueries.getList({ imageFileName: image.imageFileName! }),
   });
   const navigate = useNavigate();
 
